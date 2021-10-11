1e65e60b07cf92100b796cf4618b188ef33d65cd
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-10-10 22:02:50 -0700

    I was going to give up, but this one weird trick solved my threading issues.
    It's really a weird trick; I need to join() the protected mainLoopThread in a HeadlessApplication so PicoCLI won't run past it and quit.

--- core/src/main/java/com/github/tommyettinger/NoiseGen.java
+++ core/src/main/java/com/github/tommyettinger/NoiseGen.java
@@ -1,10 +1,15 @@
 package com.github.tommyettinger;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.PixmapIO;
 
 public class NoiseGen extends ApplicationAdapter {
 
     public Noise noise;
+    public int width = 512;
+    public int height = 512;
 
     public NoiseGen() {
         noise = new Noise();
@@ -12,10 +17,23 @@ public class NoiseGen extends ApplicationAdapter {
     public NoiseGen(Noise n) {
         noise = n;
     }
+    public NoiseGen(Noise n, int w, int h) {
+        noise = n;
+        width = w;
+        height = h;
+    }
 
     @Override
     public void create() {
-
+        Pixmap pm = new Pixmap(width, height, Pixmap.Format.RGB888);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int v = (int)(noise.getConfiguredNoise(x, y) * 127.999f + 127.999f);
+                pm.drawPixel(x, y, v * 0x010101 << 8 | 255);
+            }
+        }
+        PixmapIO.writePNG(Gdx.files.local("noise.png"), pm);
+        System.out.println("Done!");
         System.exit(0);
     }
 }
--- headless/src/main/java/com/github/tommyettinger/headless/HeadlessLauncher.java
+++ headless/src/main/java/com/github/tommyettinger/headless/HeadlessLauncher.java
@@ -3,6 +3,8 @@ package com.github.tommyettinger.headless;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.math.MathUtils;
+import com.github.tommyettinger.Noise;
 import com.github.tommyettinger.NoiseGen;
 import picocli.CommandLine;
 
@@ -13,6 +15,12 @@ import java.util.concurrent.Callable;
 		mixinStandardHelpOptions = true)
 public class HeadlessLauncher implements Callable<Integer> {
 
+	public Noise noise = new Noise();
+	@CommandLine.Option(names = {"-f", "--frequency"}, description = "The frequency of the noise, with high frequency changing rapidly.", defaultValue = "0.03125")
+	public float frequency = 0x1p-5f;
+	@CommandLine.Option(names = {"-s", "--seed"}, description = "The seed that determines how the noise will form using the given parameters.")
+	public int seed = (int) MathUtils.random(0xFFFFFFFFL);
+
 	public static void main(String[] args) {
 		int exitCode = new picocli.CommandLine(new HeadlessLauncher()).execute(args);
 		System.exit(exitCode);
@@ -22,7 +30,17 @@ public class HeadlessLauncher implements Callable<Integer> {
 	public Integer call() {
 		HeadlessApplicationConfiguration configuration = new HeadlessApplicationConfiguration();
 		configuration.updatesPerSecond = -1;
-		new HeadlessApplication(new NoiseGen(), configuration);
+		noise.setFrequency(frequency);
+		noise.setSeed(seed);
+		new HeadlessApplication(new NoiseGen(noise), configuration){
+			{
+				try {
+					mainLoopThread.join(30000L);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		};
 		return 0;
 	}
 }
