65b1cd712a5150335493586e98e8b9ee559272c9
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-09-23 15:58:50 -0700

    Update the Graal-launching files.

--- graalvm-env/build_native.bat
+++ graalvm-env/build_native.bat
@@ -1,5 +1,5 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
-call C:\d\jvm\graal17\bin\native-image.cmd ^
+C:\d\jvm\graal17\bin\native-image.cmd ^
+-H:IncludeResources="(.*\.dll)|(com/badlogic/gdx/utils/.*\.(png|fnt))" ^
 -H:+ReportExceptionStackTraces ^
 --report-unsupported-elements-at-runtime ^
 --no-fallback ^
@@ -9,5 +9,5 @@ call C:\d\jvm\graal17\bin\native-image.cmd ^
 -H:DynamicProxyConfigurationFiles=config/proxy-config.json ^
 -H:SerializationConfigurationFiles=config/serialization-config.json ^
 -H:ResourceConfigurationFiles=config/resource-config.json ^
--Dorg.lwjgl.librarypath=. ^
--jar noisegen-0.1.2.jar
+-march=compatibility ^
+-jar ../headless/build/lib/noisegen-0.1.3.jar
